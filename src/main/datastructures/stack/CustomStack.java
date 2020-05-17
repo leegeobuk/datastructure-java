63d5e37f9ffb45f5f9b7a1c606d1f8c3ecf728f4
Dong Min Lee <leegeobuk@gmail.com>
2020-05-18 02:03:26 +0900

    Initial commit for Stack

---
+++ src/main/datastructures/stack/CustomStack.java
@@ -0,0 +1,5 @@
+package main.datastructures.stack;
+
+public interface CustomStack<E> {
+
+}
---
+++ src/main/datastructures/stack/Stack.java
@@ -0,0 +1,5 @@
+package main.datastructures.stack;
+
+public class Stack<E> implements CustomStack<E> {
+
+}
