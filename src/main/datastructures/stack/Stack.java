fa8194ac63c13e540fb76d3724616e0e3fd4e20e
Dong Min Lee <leegeobuk@gmail.com>
2020-05-18 16:03:09 +0900

    Feat empty, push, pop, peek and search

--- src/main/datastructures/stack/Stack.java
+++ src/main/datastructures/stack/Stack.java
@@ -1,5 +1,49 @@
 package main.datastructures.stack;
 
+import main.datastructures.linkedlist.doubly.DoublyLinkedList;
+
 public class Stack<E> implements CustomStack<E> {
 
+    private DoublyLinkedList<E> linkedList;
+
+    @Override
+    public boolean empty() {
+        return linkedList.isEmpty();
+    }
+
+    @Override
+    public E peek() {
+        return linkedList.getLast();
+    }
+
+    @Override
+    public E pop() {
+        return linkedList.removeLast();
+    }
+
+    @Override
+    public E push(E e) {
+        linkedList.append(e);
+        java.util.Stack<E> stack = new java.util.Stack<>();
+        return e;
+    }
+
+    /**
+     * Returns the 1-based position where an element is on this stack.
+     * If the element e occurs as an item in this stack, this
+     * method returns the distance from the top of the stack of the
+     * occurrence nearest the top of the stack; the topmost item on the
+     * stack is considered to be at distance 1. The equals
+     * method is used to compare e to the items in this stack.
+     *
+     * @param   e    the desired element
+     * @return  the 1-based position from the top of the stack where
+     *          the element is located; the return value -1
+     *          indicates that the element is not on the stack
+     */
+    @Override
+    public int search(E e) {
+        int index = linkedList.indexOf(e);
+        return index >= 0 ? (linkedList.size() - index) : -1;
+    }
 }
